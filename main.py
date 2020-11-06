ffa4df0034a28c72ce9340e78c8609dd4461b736
John Doe <sisoovesimhoo@gmail.com>
2020-11-06 13:07:26 +0200

    LUI updated, critical bugs fixed, code more generic.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-for sys import exit
+from sys import exit
 from random import choice, shuffle
 from docx import Document
 from docx.shared import Pt
@@ -11,7 +11,11 @@ from email import encoders
 import smtplib
 
 
-##### ---------- Functions ---------- #####
+##### ---------- GUI ---------- #####
+
+pass
+
+##### ---------- LUI ---------- #####
 
 def abort():
 	"""
@@ -20,13 +24,13 @@ def abort():
 	
 	exit()
 
-def user_input():
+def user_input(operations_dict):
 	"""
 		The function receives and validates all the inputs that the user enters.
 	"""
 	
 	# Ask for a minimum number.
-	range_min = int(input("Please enter a minimum number: "))
+	range_min = input("Please enter a minimum number: ")
 	
 	# Make sure that the received input is a number.
 	if not range_min.isdigit():
@@ -34,23 +38,30 @@ def user_input():
 		abort()
 
 	# Ask for a maximum number.
-	range_max = int(input("Please enter a maximum number: "))
+	range_max = input("\nPlease enter a maximum number: ")
 	
 	# Make sure that the received input is a number.
 	if not range_max.isdigit():
 		print("Next time enter a number please.")
 		abort()
 	
+	# Convert range min and range max to int numbers.
+	range_min = int(range_min)
+	range_max = int(range_max)
+	
 	# Ask for exercises count.
-	amount = int(input("Please indicate the number of exercises you wish to generate: "))
+	amount = input("\nPlease indicate the number of exercises you wish to generate: ")
 	
 	# Make sure that the received input is a number > 0.
-	if not amount.isdigit() or amount.isdigit() and not amount > 0:
+	if not amount.isdigit() or amount.isdigit() and not int(amount) > 0:
 		print("Next time enter a number greater than zero please.")
 		abort()
+		
+	# Convert amount to int number.
+	amount = int(amount)
 	
 	# Ask for operations type.
-	operations = input("Please indicate what operations you would like to have in the exercises.\nm-multiplication, d-division.\nYou can specify more than one operation, for example: dm (both ':' and 'x').\nOperations: ")
+	operations = input("\nPlease indicate what operations you would like to have in the exercises.\n\nm-multiplication\nd-division.\n\n- You can specify more than one operation, for example: dm (both ':' and 'x').\n\nOperations: ")
 	
 	# Make sure all the received operations are available.
 	for chr in operations:	
@@ -59,7 +70,7 @@ def user_input():
 			abort()
 	
 	# Ask for the document name.
-	file_name = input("worksheet name: ")
+	file_name = input("\nworksheet name: ")
 	
 	# If the file name has a dot cut the name until the dot.
 	if '.' in file_name: file_name = file_name[:file_name.index('.')]
@@ -68,11 +79,11 @@ def user_input():
 	file_path = os.path.join("Exercises", file_name + '.docx')
 	
 	# If the file name already exist, replace it with the new one.
-	if os.path.exist(file_path):
+	if os.path.exists(file_path):
 		os.remove(file_path)
 	
 	# Ask for the desired output: p for print or m for mail.
-	action = input("Please indicate in what form you want to get the file.\nEnter 'p' for direct print or 'm' to get the file in mail.")
+	action = input("\nPlease indicate in what form you want to get the file.\n\np- direct print\nm- Send document to mail.\n\n- You can specify more than one action, for example: mp (both printing and mailing).\n\nActions: ")
 	
 	# Make sure an available action received.
 	if not 'p' in action and not 'm' in action:
@@ -83,7 +94,7 @@ def user_input():
 	if 'm' in action:
 		
 		# Get the mail address of the user.
-		mail_address = input("Please enter your mail address: ")
+		mail_address = input("\nPlease enter your mail address: ")
 		
 		# Make a minimal check that a valid mail address was entered.
 		if not '@' in mail_address:
@@ -95,9 +106,16 @@ def user_input():
 	
 		mail_address = None
 	
+	# Indicate the user that the all the input was received.
+	print('\n\nAll input received successfully.\n')
+	
 	# Return all the inputs.
 	return range_min, range_max, amount, operations, file_name, file_path, action, mail_address
 
+	
+##### ---------- Functions ---------- #####
+	
+	
 def generate_riddles(amount, operation, range_min, range_max):
 	"""
 		The function receives operation type and amount of riddles to create and returns a list of operations.
@@ -132,11 +150,80 @@ def generate_riddle(operation, range_min, range_max):
 		return str(num1 * num2) + ' : ' + str(num1) + '  = '
 		
 
+def create_exercises(operations, amount, range_min, range_max):
+	"""
+		The function creates and randomizes the desired exercises.
+	"""
+	
+	# The list with all the exercises.
+	riddles = []
+	
+	# Check if more than one operation selected.
+	if len(operations) > 1:
+		amount = int(round(amount / len(operations)))
+
+	# Check if the operation of the exercises is division.
+	if "d" in operations:
+		
+		riddles += generate_riddles(amount, 'd', range_min, range_max)
+
+	# Create multiplication exercises as default.
+	if 'm' in operations:
+		
+		riddles += generate_riddles(amount, 'm', range_min, range_max)
+	
+	# Shuffle all the exercises.
+	shuffle(riddles)
+	
+	# Return all the exercises as a list.
+	return riddles
+
+
+def format_riddles(riddles):
+	"""
+		The function receives exercises list and formats them to a string.
+	"""
+	
+	# Format the riddles.
+	riddles_formatted = '\n\n\n'.join(riddles[i] + '\t\t\t\t\t\t\t' + riddles[i - 1] for i in range(1, len(riddles), 2))
+
+	# If odd number of exercises add the last one.
+	if not len(riddles_formatted) %2 == 0: riddles_formatted += '\n\n\n' + riddles[-1]
+	
+	# Return the exercises string.
+	return riddles_formatted
+	
+
+def save_as_word(operations_dict, operations, range_min, range_max, riddles_formatted, file_path):
+	"""
+		The function saves the exercises as a word document.
+	"""
+	
+	# Create the word document.
+	document = Document()
+
+	# Add header to the document.
+	document.add_heading((', '.join(operations_dict[operation] for operation in operations)).replace(', ', " and ", -1) + ' of numbers from ' + str(range_min) + ' to ' + str(range_max) + '\n\n', 0)
+
+	# Define the font size.
+	style = document.styles['Normal']
+	font = style.font
+	font.size = Pt(14)
+
+	# Add the exercises to the document.
+	document.add_paragraph(riddles_formatted, style='Normal')
+
+	# Save the document.
+	document.save(file_path)
+	
+	
 def send_mail(mail_address, file_name, file_path):
 	"""
 		The function sends the document by mail.
 	"""
-
+	
+	print("Preparing mail to be sent... ", end="")
+	
 	fromaddr = 'classifiedanonymouse@gmail.com'
 	toaddr = mail_address
 
@@ -161,89 +248,73 @@ def send_mail(mail_address, file_name, file_path):
 		server.ehlo()
 		server.starttls()
 		server.login(fromaddr, 'ThisIsClassified4@')
-		server.sendmail(fromaddr, toaddr, msg)
+		# server.sendmail(fromaddr, toaddr, msg)
 		server.quit()
 		
-		print('Email sent successfully')
+		print('Email sent successfully.')
 
 	except Exception as e:
-		print("Email couldn't be sent")
+		print("Email couldn't be sent.")
+		
+		
+def execute_actions(action, mail_address, file_name, file_path):
+	"""
+		receive the word document and choose what to do with it.
+	"""
+	
+	# If the user want the document by mail send it to his mail address.
+	if 'm' in action:
+
+		# Send the document to mail.
+		send_mail(mail_address, file_name, file_path)
+
+	# If the user want the document to be printed, send it to the printer.
+	if 'p' in action:
+		
+		print("Preparing document to print... ", end="")
+		
+		try:
+		
+			# Print the word document.
+			#os.startfile(os.path.join("Exercises", file_name + '.docx'), "print")
+		
+			print("Document Sent to printer successfully.")
+			
+		except Exception as e:
+		
+			print("Document couldn't be sent to printer.")
 	
 
 ##### ---------- Create exercises ---------- #####
 
 
-# ----- inputs -----
-
-# Get all the necessary inputs from the user.
-# range_min, range_max, amount, operations, file_name, file_path, action, mail_address = user_input()
-
-range_min = 2
-range_max = 12
-amount = 100
-operations = 'md'
-file_name = 'ws1'
-file_path = os.path.join("Exercises", file_name + '.docx')
-if os.path.exists(file_path): os.remove(file_path)
-action='p'
-
-# Will contain all the exercises.
-riddles = []
-
-# For the header in the document.
+# Indicates what operations are available and their short-cuts.
 operations_dict = {'m': "Multiplication", 'd': "Division"}
 
-# Check if more than one operation selected.
-if len(operations) > 1:
-	amount = int(round(amount / len(operations)))
+# Get all the necessary inputs from the user.
+range_min, range_max, amount, operations, file_name, file_path, action, mail_address = user_input(operations_dict)
 
-# Check if the operation of the exercises is division.
-if "d" in operations:
-	
-	riddles += generate_riddles(amount, 'd', range_min, range_max)
+print("Creating exercises... ", end="")
 
-# Create multiplication exercises as default.
-if 'm' in operations:
-	
-	riddles += generate_riddles(amount, 'm', range_min, range_max)
+# Will contain all the exercises.
+riddles = create_exercises(operations, amount, range_min, range_max)
 
-shuffle(riddles)
-		
-#### ---------- Save and print ---------- #####
+print("complete.\n")
+print("formatting exercises... ", end="")
 
-# Format the riddles.
-riddles_formatted = '\n\n\n'.join(riddles[i] + '\t\t\t\t\t\t\t' + riddles[i - 1] for i in range(1, len(riddles), 2))
+# Format all the exercises to a string.
+riddles_formatted = format_riddles(riddles)
 
-# If odd number of exercises add the last one.
-if not len(riddles_formatted) %2 == 0: riddles_formatted += '\n\n\n' + riddles[-1]
+print("complete.\n")
+print("\nSaving exercises to a word document... ", end="")
 
-# ----- Word print ----- #
+# Save the exercises as a word document.
+save_as_word(operations_dict, operations, range_min, range_max, riddles_formatted, file_path)
 
-# Create the word document.
-document = Document()
+print("complete.\n")
+print("Executing action:")
 
-# Add header to the document.
-document.add_heading((', '.join(operations_dict[operation] for operation in operations)).replace(', ', " and ", -1) + ' of numbers from ' + str(range_min) + ' to ' + str(range_max) + '\n\n', 0)
+# Execute the actions that the user wishes to do with the document.
+execute_actions(action, mail_address, file_name, file_path)
 
-# Define the font size.
-style = document.styles['Normal']
-font = style.font
-font.size = Pt(14)
-
-# Add the exercises to the document.
-document.add_paragraph(riddles_formatted, style='Normal')
-
-# Save the document.
-document.save(file_path)
-
-# If the user want the document by mail send it to his mail address.
-if 'm' in action:
-
-	# Send the document to mail.
-	send_mail(mail_address, file_name, file_path)
-
-# If the user want the document to be printed, send it to the printer.
-if 'p' in action:
-
-	# Print the word document.
-	os.startfile(os.path.join("Exercises", file_name + '.docx'), "print")
+print("\nAll actions are done successfully, thank you for using Dynamic Math.\nCome again soon.")
